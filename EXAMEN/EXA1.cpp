dd85d1d6c17c9941c1259765339a6f647da71b63
DannyAVM01 <125704824+DannyAVM01@users.noreply.github.com>
2023-10-20 16:42:09 +0000

    EXAMEN

---
+++ EXAMEN/EXA1.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file EXA1.cpp
+ * @author Victorio Medina Danny Alejandro (dvictoriom@alumno.ipn.mx)
+ * @boleta: 2023640637
+ * @brief EXAMEN SEGUNDA UNIDAD
+ * @version 1.0
+ * @date 2023-10-20
+ * @copyright Copyright (c) 2023
+ */
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+    return 0;
+}
